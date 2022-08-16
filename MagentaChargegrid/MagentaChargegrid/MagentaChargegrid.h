4c707d3e8a994a4936b62da1eacd7f9abb6aa986
magenta-developer <developer.magentagroup.com@gmail.com>
2022-08-16 17:50:59 +0530

    testing

---
+++ MagentaChargegrid/MagentaChargegrid/MagentaChargegrid.h
@@ -0,0 +1,18 @@
+//
+//  MagentaChargegrid.h
+//  MagentaChargegrid
+//
+//  Created by DEEPAK KUMAR on 16/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagentaChargegrid.
+FOUNDATION_EXPORT double MagentaChargegridVersionNumber;
+
+//! Project version string for MagentaChargegrid.
+FOUNDATION_EXPORT const unsigned char MagentaChargegridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagentaChargegrid/PublicHeader.h>
+
+
---
+++ MagentaChargegrid/MagentaChargegrid/services.swift
@@ -0,0 +1,18 @@
+//
+//  services.swift
+//  MagentaChargegrid
+//
+//  Created by DEEPAK KUMAR on 16/08/22.
+//
+
+import Foundation
+
+public class services {
+ 
+    public static func tempFunc() -> String {
+        
+        return "Deepak"
+        
+    }
+
+}
